9a74a109aea2c2449057c51ea7d61908d7963a80
jon.shaw <7594012+jonnyshaw89@users.noreply.github.com>
2024-10-23 16:31:24 +0100

    make instance smaller

--- freetakserver/terraform/ec2.tf
+++ freetakserver/terraform/ec2.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
   subnet_id     = element(data.aws_subnets.private.ids, 0)
 
   key_name = "JonTak"
