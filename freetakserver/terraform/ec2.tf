57b511d266dcba7da035044994d1f39d7bd58398
jon.shaw <7594012+jonnyshaw89@users.noreply.github.com>
2024-10-23 16:17:06 +0100

    put ec2 instance in subnet

--- freetakserver/terraform/ec2.tf
+++ freetakserver/terraform/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.small"
-  subnet_id = index(data.aws_subnets.private.ids, 0)
+  subnet_id     = element(data.aws_subnets.private.ids, 0)
 
   key_name = "JonTak"
 
