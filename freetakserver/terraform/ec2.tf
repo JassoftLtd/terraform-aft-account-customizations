4fb3e47a40fa096920e337d5349faec364511708
jon.shaw <7594012+jonnyshaw89@users.noreply.github.com>
2024-10-23 15:57:55 +0100

    put ec2 instance in subnet

--- freetakserver/terraform/ec2.tf
+++ freetakserver/terraform/ec2.tf
@@ -1,22 +1,8 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.small"
+  subnet_id = data.aws_subnets.private[0].id
 
   key_name = "JonTak"
 
